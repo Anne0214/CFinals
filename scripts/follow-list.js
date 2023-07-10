8d2bbdf42fe2b71e03d8b0638ffc0b2ee935ddd9
Anne0214 <63950140+Anne0214@users.noreply.github.com>
2023-07-10 16:29:16 +0800

    0710-完成大部分關注動態頁

--- scripts/follow-list.js
+++ scripts/follow-list.js
@@ -3,6 +3,18 @@ $(".sidebar-list li").click(function() {
     $(this).addClass("active").removeClass("hover").siblings().removeClass("active").addClass("hover");
 });
 
+$(".sidebar li").click(function(){
+    
+    if($(this).text().indexOf('追蹤動態')>=0){
+        console.log("追蹤動態");
+        $("#d-follow-content").addClass("active").siblings().removeClass("active");
+    }
+    if($(this).text().indexOf('我的追蹤')>=0){
+        console.log("我的追蹤");
+        $("#d-follow-list").addClass("active").siblings().removeClass("active");
+    }
+});
+
 // 以下待修
 $("button:contains('追蹤')").click(function() {
     console.log("追蹤變取消追蹤")
