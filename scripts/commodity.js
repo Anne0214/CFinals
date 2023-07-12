15de20bb46aee55016b8f8a534061e37badfad99
Anne0214 <63950140+Anne0214@users.noreply.github.com>
2023-07-12 22:51:35 +0800

    0712-商品頁完成

--- scripts/commodity.js
+++ scripts/commodity.js
@@ -13,3 +13,23 @@ $("#add_btn").click(function() {
     count++;
     $("#num").val(count);
 })
+
+//tab
+$(".tab a").click(function(event) {
+    event.preventDefault();
+    $(this).addClass("active").siblings().removeClass("active");
+    $($(this).attr("href")).show().siblings().hide();
+
+})
+
+//slider
+$(".slider-nav>img").click(
+    function() {
+        var img = $(this).attr("src")
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".slider-for img").fadeOut(300, function() {
+            $(".slider-for img").attr("src", img);
+            $(".slider-for img").fadeIn(300);
+        });
+
+    });
